0b9fd647be01fb3c386db1648861dc14a6ed195d
Cauã Jacaúna <82884472+CauaJacauna@users.noreply.github.com>
2023-08-27 18:28:30 -0300

    Atualizar o Cubo.js

--- Cubo.js
+++ Cubo.js
@@ -4,4 +4,9 @@ function cubo(num){
    // Poderia ser (num ^ 3) também 
 		return cubo;
 }
+function cubo2(num){
+  var cubo2 = (num ^ 3);
+    return cubo2;
+}
 		document.write(cubo(12));
+    document.write(cubo2(3));
