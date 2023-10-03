0ae12b5f7092b22b84912daa366fd14e4691ac3e
Cauã Jacaúna <82884472+CauaJacauna@users.noreply.github.com>
2023-10-02 22:29:15 -0300

    Update DiasOption.js

--- DiasOption.js
+++ DiasOption.js
@@ -1,4 +1,3 @@
 for (var i = 1; i <= 31; i++) {
-    document.write("<option> Dia " + i + "</option>");
+    document.write(`<option> Dia ${i} </option>`);
 }
-//Procurar forma de implementar melhor isso
