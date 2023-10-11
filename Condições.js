15b37e3257e7b7913ca13b677f1b10f70faebb6e
Cauã Jacaúna <jacaunacaua2018@gmail.com>
2023-10-10 22:08:13 -0300

    Update Condições.js

--- Condições.js
+++ Condições.js
@@ -1,6 +1,10 @@
-var velocidade = 72.6
+var velocidade = 52.6
+
+
+console.log(`Velocidade do veículo é de ${velocidade}Km/h`)
 
 if (velocidade > 60){
-    console.log(`Velocidade do veículo é de ${velocidade}Km/h`)
-    console.log('Acima da velocidade máxima permitida, MULTADO')
+        console.log('Acima da velocidade máxima permitida, MULTADO')
 }
+
+console.log('Dirija sempre com cinto de segurança!')
