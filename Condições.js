9c8ce189fc48db79edfbd1c2bebd0448597552e9
Cauã Jacaúna <82884472+CauaJacauna@users.noreply.github.com>
2023-10-13 23:43:27 -0300

    Fibonacci

--- Condições.js
+++ Condições.js
@@ -8,3 +8,20 @@ if (velocidade > 60){
 }
 
 console.log('Dirija sempre com cinto de segurança!')
+
+
+//FIBONACCI
+
+function calcularFibonacci(n) {
+  let fibonacci = [0, 1];
+
+  for (let i = 2; i < n; i++) {
+    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+  }
+
+  return fibonacci;
+}
+
+const n = 10; // Substitua por quantos números da série você deseja calcular
+const resultado = calcularFibonacci(n);
+console.log(resultado);
