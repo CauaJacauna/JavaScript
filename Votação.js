035dd29d0d0724c0f418c08c91dcfe69ed14631a
Cauã Jacaúna <jacaunacaua2018@gmail.com>
2023-10-19 14:43:35 -0300

    Votação.js
    Exercício básico de condição

---
+++ Votação.js
@@ -0,0 +1,9 @@
+var eleitor = 18
+
+if ( eleitor >= 18) {
+    console.log('Voto obrigatório!')
+} else if ( eleitor >=16 && eleitor < 18 ) {
+    console.log('Voto opcional!')
+} else {
+    console.log('Não vota!')
+}
